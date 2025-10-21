d31a4682a4c3ed4f3ef6aee05a39ef1349dcfc0b
Furecon <kahlcostore@gmail.com>
2025-10-21 16:07:46 +0300

    Updated package-lock.json

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.kolayfit.app',
+  appId: 'com.kolayfitai.app',
   appName: 'KolayFit',
   webDir: 'dist',
   server: {
