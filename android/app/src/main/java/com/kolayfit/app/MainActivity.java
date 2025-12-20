00282da50eac3f4f4260d86757ff4c6987cc17e4
Furecon <kahlcostore@gmail.com>
2025-12-20 09:59:42 +0300

    Updated build.gradle

--- android/app/src/main/java/com/kolayfit/app/MainActivity.java
+++ android/app/src/main/java/com/kolayfit/app/MainActivity.java
@@ -1,18 +1,11 @@
 package com.kolayfit.app;
 
 import android.os.Bundle;
-import android.util.Log;
 import com.getcapacitor.BridgeActivity;
-import com.codetrixstudio.capacitor.GoogleAuth.GoogleAuth;
 
 public class MainActivity extends BridgeActivity {
-    private static final String TAG = "MainActivity";
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        Log.d(TAG, "Registering GoogleAuth plugin");
-        registerPlugin(GoogleAuth.class);
         super.onCreate(savedInstanceState);
-        Log.d(TAG, "MainActivity onCreate completed");
     }
 }
