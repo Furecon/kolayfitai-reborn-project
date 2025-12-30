5838ad120b0e5d183ff609c84475e995d3119046
Furecon <kahlcostore@gmail.com>
2025-12-30 14:13:44 +0300

    Updated styles.xml

--- android/app/src/main/java/com/kolayfit/app/MainActivity.java
+++ android/app/src/main/java/com/kolayfit/app/MainActivity.java
@@ -1,11 +1,18 @@
 package com.kolayfit.app;
 
+import android.os.Build;
 import android.os.Bundle;
+import androidx.core.view.WindowCompat;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Enable edge-to-edge for Android 15+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.VANILLA_ICE_CREAM) {
+            WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
+        }
     }
 }
